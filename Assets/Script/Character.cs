4f2f0bece123031673f5c63b3ffc6e48bd39622a
Nope <trash@mail.com>
2020-06-01 14:02:47 +0900

    Character
    Stats

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private float m_Strength = 10;
+    private float m_Magic = 10;
+    private float m_Defense = 10;
+    private float m_Resistance = 10;
+    private float m_Evade = 10;
+    private float m_CritChance = 0;
+    private float m_CritMultiplier = 1.5f;
+    private float m_MoveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
