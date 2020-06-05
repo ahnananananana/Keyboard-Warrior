cae22cfb1dd6f52c0aac2e3dc8fc9c41e8ab3ddb
ahnananananana <hurjm1004@naver.com>
2020-06-05 13:05:33 +0900

    test

--- Assets/Script/StatList.cs
+++ Assets/Script/StatList.cs
@@ -29,25 +29,24 @@ public class StatList : StatManager
     private StatManager m_EXP;
     private StatManager m_Level;
 
+    public StatManager MaxHP { get => m_MaxHP; set => m_MaxHP = value; }
+    public StatManager MaxMP { get => m_MaxMP; set => m_MaxMP = value; }
+    public StatManager HP { get => m_HP; set => m_HP = value; }
+    public StatManager MP { get => m_MP; set => m_MP = value; }
+    public StatManager Strength { get => m_Strength; set => m_Strength = value; }
+    public StatManager Magic { get => m_Magic; set => m_Magic = value; }
+    public StatManager PhysicalAtk { get => m_PhysicalAtk; set => m_PhysicalAtk = value; }
+    public StatManager MagicAtk { get => m_MagicAtk; set => m_MagicAtk = value; }
+    public StatManager Defense { get => m_Defense; set => m_Defense = value; }
+    public StatManager Resistance { get => m_Resistance; set => m_Resistance = value; }
+    public StatManager Evade { get => m_Evade; set => m_Evade = value; }
+    public StatManager Accuracy { get => m_Accuracy; set => m_Accuracy = value; }
+    public StatManager CritChance { get => m_CritChance; set => m_CritChance = value; }
+    public StatManager CritMultiplier { get => m_CritMultiplier; set => m_CritMultiplier = value; }
+    public StatManager MoveSpeed { get => m_MoveSpeed; set => m_MoveSpeed = value; }
+    public StatManager EXP { get => m_EXP; set => m_EXP = value; }
+    public StatManager Level { get => m_Level; set => m_Level = value; }
+
     //각 스텟에 접근해서 setStat하기 위한 get 함수들
-    public StatManager MaxHP()           { return m_MaxHP; }
-    public StatManager MaxMP()           { return m_MaxMP; }
-    public StatManager HP()              { return m_HP; }
-    public StatManager MP()              { return m_MP; }
 
-    public StatManager Strength()        { return m_Strength; }
-    public StatManager Magic()           { return m_Magic; }
-    public StatManager PhysicalAtk()     { return m_PhysicalAtk; }
-    public StatManager MagicAtk()        { return m_MagicAtk; }
-    public StatManager Defense()         { return m_Defense; }
-    public StatManager Resistance()      { return m_Resistance; }
-
-    public StatManager Evade()           { return m_Evade; }
-    public StatManager Accuracy()        { return m_Accuracy; }
-    public StatManager CritChance()      { return m_CritChance; }
-    public StatManager CritMultiplier()  { return m_CritMultiplier; }
-
-    public StatManager MoveSpeed()       { return m_MoveSpeed; }
-    public StatManager EXP()             { return m_EXP; }
-    public StatManager Level()           { return m_Level; }
 }
---
+++ Assets/Scripts/StatUI/hStatUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hStatUI : MonoBehaviour
+{
+    private StatList m_CurStat;
+    [SerializeField]
+    private Transform m_StatContainer;
+    private hStatElement[] m_StatUIElementList;
+
+    private void Awake()
+    {
+        m_StatUIElementList = m_StatContainer.GetComponentsInChildren<hStatElement>();
+
+    }
+
+    public void LoadStatWindow(StatList inStat)
+    {
+        m_CurStat = inStat;
+        RefreshUI();
+    }
+
+    public void RefreshUI()
+    {
+
+    }
+
+}
