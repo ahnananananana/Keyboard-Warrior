6d2d3dab3fe8bf068a379eb84a0315775dcfff65
LeeGilsu <lgs731@naver.com>
2020-06-19 01:32:17 +0900

    몬스터

--- Assets/GillSu/SceneTest/Script/EnemyCltr.cs
+++ Assets/GillSu/SceneTest/Script/EnemyCltr.cs
@@ -29,10 +29,10 @@ public class EnemyCltr : Monster
     {
        // if (this.t_Text.gameObject != null) Destroy(this.t_Text.gameObject, 1.0f);
     }
-    void OnHitTextOn9(float dmg)
+    void OnHitTextOn9()
     {
         t_Text = (Instantiate(Resources.Load("frepab/Hit")) as GameObject).GetComponent<HitText>();
-        t_Text.GetComponentInChildren<Text>().text = dmg.ToString(); 
+       // t_Text.GetComponentInChildren<Text>().text = ("Damage"); 
         if (t_Text != null)
         {
             t_Text.SetPosition(transform.Find("DamgeLog"), 0.15f);
@@ -42,8 +42,9 @@ public class EnemyCltr : Monster
     void Start()
     {
          tr = GetComponent<Transform>();
+
         PlayerTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
+        points = GameObject.Find("SpawnPoints").GetComponentsInChildren<Transform>();
         anim = GetComponent<Animator>();
     }
 
@@ -51,30 +52,45 @@ public class EnemyCltr : Monster
     void Update()
     {
         float dist = Vector3.Distance(tr.position, PlayerTr.position); // 플레이어 캐릭터와 몬스터의 거리를 확인
-        if (dist <= 1.0f)
-        {
-            isAttack = true;
-        }
-        else if (dist <= 5.0f) // 플레이어와 몬스터의 거리가 5.0보다 가까울경우
-        {
-            isAttack = false;
-            movePos = PlayerTr.position; // 몬스터의 위치를 플레이어의 위치로 이동한다.
-        }
-        else {
-            movePos = points[nextIdx].position; // 거리가 멀 경우  else 몬스터의 위치는 다음 인덱스 포지션으로 이동.
-            isAttack = false;
-        }
-        anim.SetBool("isAttack", isAttack);
-       
-        if (!isAttack)
-        {
+            if (dist <= 1.0f)
+            {
+                isAttack = true;
+                anim.SetBool("isIdle", false);
+
+            }
+            else if (dist <= 5.0f) // 플레이어와 몬스터의 거리가 5.0보다 가까울경우
+            {
+                isAttack = false;
             Quaternion rot = Quaternion.LookRotation(movePos - tr.position); //가야할 방향에서 현재 자기 자신의 위치를 연산
             tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
             tr.Translate(Vector3.forward * Time.deltaTime * m_speed);
-        }
+            movePos = PlayerTr.position; // 몬스터의 위치를 플레이어의 위치로 이동한다.
+            }
+            else
+            {
+            Quaternion rot = Quaternion.LookRotation(movePos - tr.position); //가야할 방향에서 현재 자기 자신의 위치를 연산
+            tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+            tr.Translate(Vector3.forward * Time.deltaTime * m_speed);
+            //movePos = points[nextIdx].position; // 거리가 멀 경우  else 몬스터의 위치는 다음 인덱스 포지션으로 이동.
+            anim.SetBool("isIdle", false);
+            movePos = points[0].position;
+               if (tr.transform.position == movePos)
+                {
+                    anim.SetBool("isIdle", true);
+                    isAttack = false;
+                }
+            }
+            anim.SetBool("isAttack", isAttack);
+            anim.SetBool("isidle", false);
 
+        /*if (!isAttack)
+            {
+                Quaternion rot = Quaternion.LookRotation(movePos - tr.position); //가야할 방향에서 현재 자기 자신의 위치를 연산
+                tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+                tr.Translate(Vector3.forward * Time.deltaTime * m_speed);
+            }*/
+    }    
 
-    }
     void OnTriggerEnter(Collider coll) //트리거가 웨이포인트에 충돌했을 시 발생함.
     {
         if (coll.tag == "WAY_Point")
@@ -89,8 +105,8 @@ public class EnemyCltr : Monster
 
         Destroy(muzzle, 0.5f);
         //stat.DealDamage(PlayerTr.GetComponent<Player>());
-      
-        OnHitTextOn9(stat.m_Damage);
+        
+        OnHitTextOn9();
     }
  
 }
--- Assets/GillSu/Script/LEEScript/Charater/HitText.cs
+++ Assets/GillSu/Script/LEEScript/Charater/HitText.cs
@@ -66,6 +66,6 @@ public class HitText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * Time.deltaTime * 2f);
+        //transform.Translate(Vector3.up * Time.deltaTime * 2f);
     }
 }
