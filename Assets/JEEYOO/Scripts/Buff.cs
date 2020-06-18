3ab0864766edd5deeb1373a3155ecee04c5c16d3
potentialk0 <64459852+potentialk0@users.noreply.github.com>
2020-06-19 00:56:00 +0900

    ㅁㄴㅇ

--- Assets/JEEYOO/Scripts/Buff.cs
+++ Assets/JEEYOO/Scripts/Buff.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Buff : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public virtual void ApplyBuff(Character c) { }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public virtual void RemoveBuff(Character c) { }
+    
 }
