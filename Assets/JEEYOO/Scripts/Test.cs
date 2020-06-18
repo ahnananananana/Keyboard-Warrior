fcb98c23337196fd73363925fcab5e23f1da25ec
potentialk0 <64459852+potentialk0@users.noreply.github.com>
2020-06-19 02:13:18 +0900

    버프추가 1.0

--- Assets/JEEYOO/Scripts/Test.cs
+++ Assets/JEEYOO/Scripts/Test.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public Player player;
+    public Player player2;
     public Monster monster;
     public MainData mainData;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
+        
         Debug.Log("player atk ==" + player.m_Attack.m_CurrentValue);
-        Debug.Log("monster atk ==" + monster.m_Attack.m_CurrentValue);
+        Debug.Log("player2 atk ==" + player.m_Attack.m_CurrentValue);
     }
 
     // Update is called once per frame
@@ -29,9 +32,10 @@ public class Test : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.F1))
         {
+            
             mainData.BuffData[0].ApplyBuff(player);
             Debug.Log("player atk == " + player.m_Attack.m_CurrentValue);
-            Debug.Log("monster atk == " + monster.m_Attack.m_CurrentValue);
+            Debug.Log("player2 atk == " + player.m_Attack.m_CurrentValue);
         }
         if (Input.GetKeyDown(KeyCode.F2))
         {
@@ -39,5 +43,7 @@ public class Test : MonoBehaviour
             Debug.Log("player atk == " + player.m_Attack.m_CurrentValue);
             Debug.Log("monster atk == " + monster.m_Attack.m_CurrentValue);
         }
+
+
     }
 }
